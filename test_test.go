cab1bf9646cb10006e4f7234c8abeb7cb84abed5
Arvinderpal Wander <awander@gmail.com>
2019-03-12 16:37:38 -0700

    This commit brings the following features: 1. allows for tests can be run against an existing local vagrant cluster 2. includes some initial basic tests for router and service-proxy functions

---
+++ test_test.go
@@ -0,0 +1,8 @@
+package kuberouterTest
+
+import (
+	// test sources
+	_ "github.com/cloudnativelabs/kube-router-e2e/router"
+	_ "github.com/cloudnativelabs/kube-router-e2e/runtime"
+	_ "github.com/cloudnativelabs/kube-router-e2e/service-proxy"
+)
